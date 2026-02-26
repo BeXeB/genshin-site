1c55d1747b661cb51cf3732a37a723e5c5232c1d
Bence Imre Kiss <kissbenceimre99@gmail.com>
2026-02-26 17:13:14 +0100

    homepage and some css changes so its consistent

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  quickAccesses = [
+    {
+      title: 'Tier List',
+      description: 'Karakterek rangsorolása erősség és szerepkör szerint.',
+      link: '/tierlist',
+    },
+    {
+      title: 'Karakterek',
+      description: 'Részletes útmutatók és információk a karakterekről.',
+      link: '/characters',
+    },
+    {
+      title: 'Egyébb Útmutatók',
+      description: 'Hasznos tippek és játékmechanikai magyarázatok.',
+      link: '/guides',
+    },
+    {
+      title: 'Eszközök',
+      description: 'Praktikus kalkulátorok és segédprogramok a játékhoz.',
+      link: '/tools',
+    },
+  ];
 }
