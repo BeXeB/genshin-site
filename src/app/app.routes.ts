c60da24dfe89ecf8976bc7695ca85279f07991bf
Bence Imre Kiss <kissbenceimre99@gmail.com>
2026-02-27 20:04:18 +0100

    fix constellation display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,31 +3,20 @@ import { TodoComponent } from './pages/todo/todo.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CharactersComponent } from './pages/characters/characters.component';
 import { TierlistComponent } from './pages/tierlist/tierlist.component';
-import { EndgamesComponent } from './pages/endgames/endgames.component';
 import { WeaponsComponent } from './pages/weapons/weapons.component';
 import { ArtifactsComponent } from './pages/artifacts/artifacts.component';
 import { ToolsComponent } from './pages/tools/tools.component';
-import { AbyssComponent } from './pages/endgames/abyss/abyss.component';
-import { StygianComponent } from './pages/endgames/stygian/stygian.component';
-import { TheatreComponent } from './pages/endgames/theatre/theatre.component';
 import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
 import { TierlistMakerComponent } from './pages/tierlist-maker/tierlist-maker.component';
 import { GuidesComponent } from './pages/guides/guides.component';
 import { GuideDetailsComponent } from './pages/guide-details/guide-details.component';
+import { TierlistViewerComponent } from './pages/tierlist-viewer/tierlist-viewer.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'tierlist', component: TierlistComponent },
   { path: 'tierlist-maker', component: TierlistMakerComponent },
-  // {
-  //   path: 'endgames',
-  //   component: EndgamesComponent,
-  //   children: [
-  //     { path: 'abyss', component: AbyssComponent },
-  //     { path: 'stygian', component: StygianComponent },
-  //     { path: 'theatre', component: TheatreComponent },
-  //   ],
-  // },
+  { path: 'tierlist-viewer', component: TierlistViewerComponent },
   { path: 'characters', component: CharactersComponent },
   { path: 'characters/:name', component: CharacterDetailsComponent },
   { path: 'guides', component: GuidesComponent },
--- src/app/pages/character-details/character-overview/constellations/constellation-details/constellation-details.component.ts
+++ src/app/pages/character-details/character-overview/constellations/constellation-details/constellation-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ConstellationDetail } from '../../../../../_models/character';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-constellation-details',
@@ -11,4 +12,40 @@ export class ConstellationDetailsComponent {
   @Input() constellation: ConstellationDetail | undefined = undefined;
   @Input() elementColor: string | null = null;
   @Input() imageUrl: string | null = null;
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  getFormattedDescription(description: string | undefined): SafeHtml {
+    if (!description) {
+      return '';
+    }
+
+    description = description.replace(/\{LINK#[^}]+\}(.*?)\{\/LINK\}/g, '$1');
+
+    description = description.replace(
+      /<color="?(#?[0-9A-Fa-f]{3,8})"?>(.*?)<\/color>/g,
+      (_, color, text) => {
+        const normalizedColor = color.startsWith('#') ? color : `#${color}`;
+        return `<span style="color:${normalizedColor}">${text}</span>`;
+      },
+    );
+
+    description = description.replaceAll('\n', '<br>');
+
+    description = description.replaceAll(
+      /<br><br>/g,
+      `</div><div style="margin: 14px 0;">`,
+    );
+
+    description = description.replaceAll(/<br>/g, '</div><div>');
+
+    const htmlString = `<div style="margin: 0px 0px 14px;">${description}</div>`;
+
+    const formattedDescription: SafeHtml =
+      this.sanitizer.bypassSecurityTrustHtml(htmlString);
+
+    console.log(formattedDescription)
+
+    return formattedDescription;
+  }
 }
