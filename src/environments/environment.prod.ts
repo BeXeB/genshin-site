b2b47a86a92b8f87f9e2d68b95ab806f6c7a3649
Bence Imre Kiss <kissbenceimre99@gmail.com>
2026-03-23 16:12:48 +0100

    another env fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://genshin-site-api.onrender.com/',
+  apiBaseUrl: 'https://genshin-site-api.onrender.com',
   apiImageBaseUrl: 'https://genshin-site-api.onrender.com/images',
 };
