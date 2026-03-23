805f06f0bb0fd223eef587f356481ee2a44702a2
Bence Imre Kiss <kissbenceimre99@gmail.com>
2026-03-23 15:54:57 +0100

    no interceptor in prod

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 import { routes } from './app.routes';
 import { errorInterceptor } from './_interceptors/error-interceptor';
 
-const httpProviders = provideHttpClient(withInterceptors([errorInterceptor]));
+const httpProviders = environment.production
+  ? provideHttpClient()
+  : provideHttpClient(withInterceptors([errorInterceptor]));
 
 export const appConfig: ApplicationConfig = {
   providers: [
